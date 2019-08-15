4a7040f3533010836c8167bbf0b71ce19de144b8
sanketco <sanketcbe@gmail.com>
2019-08-15 21:34:03 +0530

    Adding files

---
+++ assets/js/todos.js
@@ -0,0 +1,27 @@
+//Check off Specific Todos By Clicking
+$("ul").on("click" , "li" ,function(){
+    $(this).toggleClass("completed");
+});
+
+// Click on X to delete Todo
+$("ul").on("click" , "span" ,function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        // grabbing new todotext from input
+        var todoText = $(this).val();
+        $(this).val("");
+        //create a new li and add ul
+        $("ul").append("<li><span><i class='fas fa-trash'></i></span> " + todoText + "</li>")
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+
+});
